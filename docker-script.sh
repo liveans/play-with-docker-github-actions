7881aad43bbfd0db4ec6fc45c77f63a87b5e4a34
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-08-14 02:54:46 +0200

    .NET ARM32 Workaround

--- docker-script.sh
+++ docker-script.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
+cd /main
 
 sudo apt-get update
-sudo apt-get install -y wget git libicu-dev
+sudo apt-get install -y wget git libicu-dev zlib1g-dev clang
 
 sudo apt-get install -y ./artifacts/libmsquic_*.deb
+
+cd ./artifacts/bin/linux/${1}_${2}_${3}
+sudo chmod +x msquictest
+sudo ./msquictest --gtest_filter=ParameterValidation.ValidateApi
+cd ../../../..
 
 sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 sudo chmod +x dotnet-install.sh
@@ -12,12 +18,10 @@ sudo ./dotnet-install.sh --channel 9.0
 export DOTNET_ROOT=/root/.dotnet
 export PATH=$PATH:/root/.dotnet
 
-cd artifacts/bin/linux/$1
-sudo chmod +x msquictest
-sudo ./msquictest --gtest_filter=ParameterValidation.ValidateApi
-cd ../../../..
-
-sudo git clone --depth 1 https://github.com/liveans/QuicSimpleTest.git
 cd QuicSimpleTest
-sudo dotnet build -c Release
-sudo dotnet run -c Release
+sudo dotnet ./bin/$2/net9.0/linux-${1}/QuicHello.dll
+
+# sudo git clone --depth 1 https://github.com/liveans/QuicSimpleTest.git
+# cd QuicSimpleTest
+# sudo dotnet build -c Release
+# sudo dotnet run -c Release
